3ebfedbb09ea74c370a1a4d901207a40c4110dc4
修伟 <xiuwei@sunshineftg.com>
2021-12-27 14:45:11 +0800

    添加api url配置功能

--- preload.js
+++ preload.js
@@ -1,39 +1,38 @@
-const cp = require('child_process') //调用系统命令 cp.exec('notepad');
-const https = require("https")
-let api_url = "https://flomoapp.com/iwh/MzMxMTAz/2b6518eecb84daa77b45118e7b167f50/"
+const cp = require('child_process'); //调用系统命令 cp.exec('notepad');
+const https = require("https");
+let api_url = "";
 
 function post(api_url, send) {
-    // alert(api_url)
     let options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         }
-    }
+    };
     let req = https.request(api_url, options, function(res) {
         // console.log('STATUS: ' + res.statusCode);
         // console.log('HEADERS: ' + JSON.stringify(res.headers));
         // 定义了一个post变量，用于暂存请求体的信息
-        let body = ""
-        res.setEncoding('utf8')
-            // 通过res的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+        let body = "";
+        res.setEncoding('utf8');
+        // 通过res的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
         res.on('data', function(chunk) {
-                // console.log('BODY: ' + chunk);
-                body += chunk
-            })
-            // 在res的end事件触发后，通过JSON.parse将post解析为真正的POST请求格式，然后调用传递过来的回调函数处理数据
+            // console.log('BODY: ' + chunk);
+            body += chunk;
+        });
+        // 在res的end事件触发后，通过JSON.parse将post解析为真正的POST请求格式，然后调用传递过来的回调函数处理数据
         res.on('end', function() {
             console.log("body = " + body);
             // let json = JSON.parse(body)
             // callback(json)
         })
-    })
+    });
     req.on('error', function(e) {
-        console.log('problem with request: ' + e.message)
-        alter('problem with request: ' + e.message)
-    })
-    req.write(send)
-    req.end()
+        console.log('problem with request: ' + e.message);
+        alter('problem with request: ' + e.message);
+    });
+    req.write(send);
+    req.end();
 }
 
 // let tags = [
@@ -57,7 +56,7 @@ let tags = [];
  * @returns 返回能匹配到前缀的数组
  */
 function get_next(prefix) {
-    console.log(tags)
+    // console.log(tags);
     let search_list = []
     tags.map((tag) => {
         if (tag.indexOf(prefix) == 0) {
@@ -69,7 +68,7 @@ function get_next(prefix) {
                 type: 'tags_list'
             });
         }
-    })
+    });
     return search_list
 }
 
@@ -85,10 +84,10 @@ function send_memo(content) {
     if (strs.length >= 1) {
         if (is_start_by_tag(strs[0])) {
             //更新标签数据
-            update_tags(strs[0])
+            update_tags(strs[0]);
         }
     }
-    post(api_url, JSON.stringify({ 'content': content }))
+    post(api_url, JSON.stringify({ 'content': content }));
 }
 
 /**
@@ -96,17 +95,17 @@ function send_memo(content) {
  * @param {string} new_tag 新标签
  */
 function update_tags(new_tag) {
-    let need_add = true
+    let need_add = true;
     tags.map((tag) => {
         if (tag === new_tag) { //如果存在 忽略
-            need_add = false
-            console.log("tags no need update")
+            need_add = false;
+            // console.log("tags no need update");
             return;
         }
     })
     if (need_add) {
-        tags.push(new_tag)
-        let res = utools.dbStorage.setItem("uflomo_tags", tags)
+        tags.push(new_tag);
+        let res = utools.dbStorage.setItem("uflomo_tags", tags);
     }
 }
 
@@ -116,7 +115,7 @@ function update_tags(new_tag) {
  * @returns bool
  */
 function is_start_by_tag(str) {
-    return str.indexOf("#") == 0
+    return str.indexOf("#") == 0;
 }
 
 /**
@@ -125,14 +124,44 @@ function is_start_by_tag(str) {
  * @returns boolean
  */
 function put_local_memo(memo) {
-    return true
+    return true;
 }
+
+function check_api(as_check = false) {
+    if (as_check == false) {
+        if (api_url === "") {
+            utools.showNotification('尚未配置flomo用户api url,请点击进行配置!', 'uflomo_config_api');
+            // window.utools.outPlugin();
+        }
+        return true;
+    }
+    return !(api_url === "");
+}
+
+
+function set_api_url(api_url_str) {
+    //配置api
+    //校验是否是flomo的api url
+    api_url_str = api_url_str.trim();
+    let reg = /^https:\/\/flomoapp\.com\/[\w/ ]+$/;
+    if (reg.test(api_url_str)) {
+        utools.dbStorage.setItem("uflomo_api_url", api_url_str)
+        utools.showNotification('flomo api url配置成功!');
+    } else {
+        utools.showNotification('输入的api url为: ' + api_url_str);
+        utools.showNotification('flomo api url配置失败!请检查api url是否正确');
+    }
+    window.utools.outPlugin();
+}
+
+
 window.exports = {
-    "flomo_add_width_tag": { // 注意: 键对应的是 plugin.json 中的 features.code
+    "uflomo_add_width_tag": { // 注意: 键对应的是 plugin.json 中的 features.code
         mode: 'list',
         args: {
             // 进入插件时调用（可选）
             enter: (action, callbackSetList) => {
+                check_api();
                 // 如果进入插件就要显示列表数据
                 callbackSetList([{
                     title: '添加标签',
@@ -166,24 +195,24 @@ window.exports = {
             // 用户选择列表中某个条目时被调用
             select: (action, itemData, callbackSetList) => {
                 //判断itemData.content是否存在空格
-                utools.setSubInputValue(itemData.content)
+                utools.setSubInputValue(itemData.content);
                 if (itemData.type === 'create_memo') {
-                    window.utools.hideMainWindow()
+                    window.utools.hideMainWindow();
 
                     // 发送emeo
-                    send_memo(itemData.content)
-                    utools.setSubInputValue('')
-                    callbackSetList([])
+                    send_memo(itemData.content);
+                    utools.setSubInputValue('');
+                    callbackSetList([]);
                 } else {
-                    let search_list = get_next(itemData.content)
-                    callbackSetList(search_list)
+                    let search_list = get_next(itemData.content);
+                    callbackSetList(search_list);
                 }
             },
             // 子输入框为空时的占位符，默认为字符串"搜索"
             placeholder: "创建memo带标签"
         }
     },
-    "flomo_add": {
+    "uflomo_add": {
         mode: 'list',
         args: {
             // 进入插件时调用（可选）
@@ -210,25 +239,109 @@ window.exports = {
             },
             // 用户选择列表中某个条目时被调用
             select: (action, itemData, callbackSetList) => {
-                window.utools.hideMainWindow()
-
+                window.utools.hideMainWindow();
                 // 发送emeo
-                send_memo(itemData.content)
-
+                send_memo(itemData.content);
                 // window.utools.outPlugin() //这里不能退出插件,发送post请求时异步操作
             },
             // 子输入框为空时的占位符，默认为字符串"搜索"
             placeholder: "创建memo"
         }
-    }
+    },
+    // "uflomo_custom_local_tag": {
+
+    // },
+    "uflomo_config_api": {
+        mode: "list",
+        args: {
+            // 进入插件时调用
+            enter: (action, callbackSetList) => {
+                let _list = [];
+                if (action.type == 'regex') { //直接在搜索框中输入api url
+                    //如果已经配置
+                    if (check_api(true)) {
+                        _list = [{
+                            title: '你已经配置了flomo api url,选择此项会更新api url',
+                            description: "新的api为:" + action.payload,
+                            content: action.payload,
+                            icon: 'icons/logo.png',
+                            type: 'config_api'
+                        }];
+                    } else {
+                        set_api_url(action.payload);
+                    }
+                } else {
+                    if (check_api(true)) {
+                        _list.push({
+                            title: '更新api url',
+                            description: "你已经配置了flomo api url",
+                            icon: 'icons/logo.png',
+                            type: 'config_api'
+                        });
+                    } else {
+                        _list.push({
+                            title: '输入api url',
+                            description: "配置flomo api url",
+                            icon: 'icons/logo.png',
+                            type: 'config_api'
+                        });
+                    }
+                    _list.push({
+                        title: '了解flomo api url',
+                        description: '了解flomo api url',
+                        icon: 'icons/logo.png',
+                        type: 'url',
+                        url: 'https://help.flomoapp.com/advance/api.html'
+                    })
+                    _list.push({
+                        title: '查找你的flomo api url',
+                        description: '查找你的flomo api url',
+                        icon: 'icons/logo.png',
+                        type: 'url',
+                        url: 'https://flomoapp.com/mine?source=incoming_webhook'
+                    });
+                }
+                callbackSetList(_list);
+            },
+            search: (action, searchWord, callbackSetList) => {
+                let _list = [{
+                    title: "api url为: " + searchWord,
+                    description: "配置flomo api url",
+                    content: searchWord,
+                    icon: 'icons/logo.png',
+                    type: 'config_api'
+                }];
+                callbackSetList(_list);
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                // window.utools.hideMainWindow()
+                if (itemData.type == 'config_api') {
+                    set_api_url(itemData.content);
+                } else if (itemData.type == 'url') {
+                    utools.shellOpenExternal(itemData.url);
+                    window.utools.outPlugin();
+                }
+            },
+            placeholder: "在这里输入你的flomo api url"
+        }
+    },
 }
 
 //加载插件后获取存储的标签信息
 utools.onPluginReady(() => {
     let _tags = utools.dbStorage.getItem("uflomo_tags")
     if (_tags === null || typeof _tags === 'undefined') {
-        consle.log("uflomo_tags is null")
+        consle.log("uflomo: tags is null")
     } else {
         tags = _tags
+    }
+
+    let _api_url = utools.dbStorage.getItem("uflomo_api_url")
+    console.log("api url: " + _api_url);
+    if (_api_url === null || typeof _api_url === 'undefined') {
+        consle.log("uflomo:api url is null")
+    } else {
+        api_url = _api_url
     }
 })
